4271fbc8898745cb2ed3da2621aff682cfa230ac
Kevin Malakoff <kmalakoff@gmail.com>
2024-11-22 17:46:33 -0800

    Update API for external normalization

---
+++ src/index.mjs
@@ -0,0 +1,7 @@
+import './polyfills.cjs';
+
+import spawnCallback from './spawnCallback';
+
+export default spawnCallback;
+export { default as spawn } from './spawn';
+export { default as sync } from './spawnCallbackSync';
