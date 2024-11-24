77e37d711743f7ab8694b7e4703b42f949f82f2b
Kevin Malakoff <kmalakoff@gmail.com>
2024-11-24 12:25:02 -0800

    Upgrade modules

--- dist/cjs/constants.js
+++ dist/cjs/constants.js
@@ -1,12 +1,12 @@
 "use strict";
 module.exports = {
     spawnKeys: [
-        "pid",
-        "status",
-        "signal",
-        "stdout",
-        "stderr",
-        "output"
+        'pid',
+        'status',
+        'signal',
+        'stdout',
+        'stderr',
+        'output'
     ]
 };
 /* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
--- dist/cjs/spawnCallback.js
+++ dist/cjs/spawnCallback.js
@@ -1,11 +1,11 @@
 "use strict";
-require("./polyfills.js");
-var once = require("once");
-var nextTick = require("next-tick");
-var constants = require("./constants");
-var spawn = require("./spawn");
+require('./polyfills.js');
+var once = require('once');
+var nextTick = require('next-tick');
+var constants = require('./constants');
+var spawn = require('./spawn');
 function normalize(cp, options, callback) {
-    if (typeof options === "function") {
+    if (typeof options === 'function') {
         callback = options;
         options = {};
     }
@@ -19,19 +19,19 @@ function normalize(cp, options, callback) {
     if (options.encoding) {
         if (cp.stdout) {
             res.stdout = [];
-            cp.stdout.on("data", res.stdout.push.bind(res.stdout));
+            cp.stdout.on('data', res.stdout.push.bind(res.stdout));
         }
         if (cp.stderr) {
             res.stderr = [];
-            cp.stderr.on("data", res.stderr.push.bind(res.stderr));
+            cp.stderr.on('data', res.stderr.push.bind(res.stderr));
         }
     }
     // some versions of node emit both an error and close
-    cp.on("error", function error(err) {
-        err.code === "OK" || callback(err);
+    cp.on('error', function error(err) {
+        err.code === 'OK' || callback(err);
     });
     // done
-    cp.on("close", function(status, signal) {
+    cp.on('close', function(status, signal) {
         nextTick(function closeNextTick() {
             // prepare result
             res.pid = cp.pid;
@@ -39,11 +39,11 @@ function normalize(cp, options, callback) {
             res.signal = signal;
             if (res.stdout) {
                 res.stdout = Buffer.concat(res.stdout);
-                if (options.encoding !== "buffer") res.stdout = res.stdout.toString(options.encoding);
+                if (options.encoding !== 'buffer') res.stdout = res.stdout.toString(options.encoding);
             }
             if (res.stderr) {
                 res.stderr = Buffer.concat(res.stderr);
-                if (options.encoding !== "buffer") res.stderr = res.stderr.toString(options.encoding);
+                if (options.encoding !== 'buffer') res.stderr = res.stderr.toString(options.encoding);
             }
             res.output = [
                 null,
@@ -51,27 +51,27 @@ function normalize(cp, options, callback) {
                 res.stderr
             ];
             // patch: early node on windows could return null
-            if (res.status === null) console.log("spawnCallback null status code", res);
+            if (res.status === null) console.log('spawnCallback null status code', res);
             // res.status = res.status === null ? 0 : res.status;
             // process errors
             var err = res.status !== 0 ? new Error("Non-zero exit code: ".concat(res.status)) : null;
             if (res.stderr && res.stderr.length) {
-                err = err || new Error("stderr has content");
+                err = err || new Error('stderr has content');
                 for(var key in res){
                     if (constants.spawnKeys.indexOf(key) < 0) continue;
-                    err[key] = Buffer.isBuffer(res[key]) ? res[key].toString("utf8") : res[key];
+                    err[key] = Buffer.isBuffer(res[key]) ? res[key].toString('utf8') : res[key];
                 }
             }
             err ? callback(err) : callback(null, res);
         });
     });
     // pipe input
-    if (options.input && (typeof options.input === "string" || Buffer.isBuffer(options.input))) {
+    if (options.input && (typeof options.input === 'string' || Buffer.isBuffer(options.input))) {
         cp.stdin.end(options.input);
     }
 }
 function spawnCallback(command, args, options, callback) {
-    if (typeof options === "function") {
+    if (typeof options === 'function') {
         callback = options;
         options = {};
     }
