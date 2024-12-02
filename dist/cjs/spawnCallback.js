3976e5305bfc96a8c4f13c68c8a0621aae8d10c1
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-02 06:07:34 -0800

    Upgrade modules

--- dist/cjs/spawnCallback.js
+++ dist/cjs/spawnCallback.js
@@ -80,4 +80,4 @@ function spawnCallback(command, args, options, callback) {
 }
 module.exports = spawnCallback;
 module.exports.normalize = normalize;
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
