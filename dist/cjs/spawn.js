715d23272321e9e4e55598bf5a79f85291ff44d9
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-02 21:45:15 -0800

    Upgrade modules

--- dist/cjs/spawn.js
+++ dist/cjs/spawn.js
@@ -2,4 +2,4 @@
 var major = +process.versions.node.split('.')[0];
 var spawn = major <= 7 ? require('../../assets/cross-spawn-6.0.5.js').spawn : require('cross-spawn').spawn;
 module.exports = spawn;
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
