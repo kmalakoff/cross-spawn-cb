8c6d5daf3568350d769172a57f33f0c23392ce85
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 22:43:23 -0800

    Upgrade modules

--- src/spawn.js
+++ src/spawn.js
@@ -1,5 +1,3 @@
 const major = +process.versions.node.split('.')[0];
-// TODO: fix transform file extensions for cjs
-const spawn = major <= 7 ? require('../../assets/cross-spawn-6.0.5.' + 'cjs').spawn : require('cross-spawn').spawn;
-
+const spawn = major <= 7 ? require('../../assets/cross-spawn-6.0.5.js').spawn : require('cross-spawn').spawn;
 module.exports = spawn;
