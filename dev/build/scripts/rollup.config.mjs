f63c3e810a4d63bff0734b0edb5b9fea75ff1ea7
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 23:57:58 -0800

    Upgrade modules

--- dev/build/scripts/rollup.config.mjs
+++ dev/build/scripts/rollup.config.mjs
@@ -3,10 +3,12 @@ import resolve from '@rollup/plugin-node-resolve';
 import externals from 'rollup-plugin-node-externals';
 import swc from 'ts-swc-rollup-plugin';
 
+const SKIPS = ['function-exec-sync'];
+
 export default {
   output: {
     format: 'cjs',
     name: 'crossSpawn',
   },
-  plugins: [resolve({ resolveOnly: module => !module.includes('function-exec-sync') }), commonjs(), externals({ deps: false, devDeps: false, builtinsPrefix: 'strip' }), swc()],
+  plugins: [resolve({ resolveOnly: module => SKIPS.indexOf(module) < 0 }), commonjs(), externals({ deps: false, devDeps: false, builtinsPrefix: 'strip' }), swc()],
 };
