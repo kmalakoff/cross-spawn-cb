08372f868c36e9701a8cdaf9492be207e6fdbefd
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 23:57:58 -0800

    Upgrade modules

--- dev/build/scripts/assets.mjs
+++ dev/build/scripts/assets.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import path from 'path';
+import fs from 'fs';
 import url from 'url';
 import spawn from 'cross-spawn-cb';
 
@@ -8,10 +9,23 @@ const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : 
 const src = path.join(__dirname, '..', 'src', 'index.cjs');
 const dest = path.join(__dirname, '..', '..', '..', 'assets', 'cross-spawn-6.0.5.js');
 
+const PATCH = {}
+PATCH.find = "var needsShell = !isExecutableRegExp.test(commandFile);";
+PATCH.replace = `${PATCH.find}${['\n\n', '// KM: force node to use the shell\n', "if (!needsShell && ['node', 'node.exe', 'node.cmd'].indexOf(path.basename(commandFile).toLowerCase()) >= 0) needsShell = true;\n"].join('        ')}`
+
 function build(callback) {
   const config = path.resolve(__dirname, 'rollup.config.mjs');
   const args = ['rollup', '--config', config, '--input', src, '--file', dest];
-  spawn(args[0], args.slice(1), { cwd: path.dirname(__dirname), stdio: 'inherit' }, callback);
+  spawn(args[0], args.slice(1), { cwd: path.dirname(__dirname), stdio: 'inherit' }, (err) => {
+    if (err) return callback(err);
+
+    // patch
+    try {
+      fs.writeFile(dest, fs.readFileSync(dest, 'utf8').replace(PATCH.find, PATCH.replace), 'utf8', callback);
+    } catch (err) {
+      callback(err)
+    }
+  });
 }
 
 build((err) => {
