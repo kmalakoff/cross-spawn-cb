c12e463d09ca772e5ecc965acf077f421b4ca1a2
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-27 19:27:26 -0800

    Upgrade modules

--- dev/build/scripts/assets.mjs
+++ dev/build/scripts/assets.mjs
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
 import path from 'path';
 import url from 'url';
 import spawn from 'cross-spawn-cb';
@@ -11,7 +12,14 @@ const dest = path.join(__dirname, '..', '..', '..', 'assets', 'cross-spawn-6.0.5
 function build(callback) {
   const config = path.resolve(__dirname, 'rollup.config.mjs');
   const args = ['rollup', '--config', config, '--input', src, '--file', dest];
-  spawn(args[0], args.slice(1), { cwd: path.dirname(__dirname), stdio: 'inherit' }, callback);
+  spawn(args[0], args.slice(1), { cwd: path.dirname(__dirname), stdio: 'inherit' }, (err) => {
+    if (err) return callback(err);
+
+    let contents = fs.readFileSync(dest, 'utf8');
+    contents = contents.replace('Object.assign({}, options)', `require('core-js/actual/object/assign')({}, options)`);
+    fs.writeFileSync(dest, contents, 'utf8');
+    callback();
+  });
 }
 
 build((err) => {
