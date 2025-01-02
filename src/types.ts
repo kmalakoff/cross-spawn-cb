26dedbe915f57544fa82e351953e2ac6cdcc4a8c
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-02 14:28:36 -0800

    Remove dev build lockfile and update workflow

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,14 @@
-import type { SpawnOptions as SpawnOptionsBase, SpawnSyncReturns } from 'child_process';
+/// <reference types="node" />
 
-export type { ChildProcess } from 'child_process';
-export interface SpawnOptions extends SpawnOptionsBase {
+import type * as child_process from 'child_process';
+
+export type ChildProcess = child_process.ChildProcess;
+export interface SpawnOptions extends child_process.SpawnOptions {
   encoding?: BufferEncoding;
   env?: NodeJS.ProcessEnv;
   input?: string | Buffer;
 }
-export type SpawnResult = SpawnSyncReturns<string | Buffer>;
+export type SpawnResult = child_process.SpawnSyncReturns<string | Buffer>;
 
 export interface SpawnError extends Error {
   status?: number;
