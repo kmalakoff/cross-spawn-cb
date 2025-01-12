571202935504e8029690355155c6f0d1562b751f
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-12 12:40:44 -0800

    Update build scripts and rename shims to pre.js

--- dev/build/scripts/assets.mjs
+++ dev/build/scripts/assets.mjs
@@ -3,22 +3,26 @@
 import path from 'path';
 import url from 'url';
 import spawn from 'cross-spawn-cb';
+import Queue from 'queue-cb';
+import resolve from 'resolve';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 const cwd = process.cwd();
-const src = path.join(__dirname, '..', 'node_modules', 'cross-spawn-6.0.5', 'index.js');
-const dest = path.join(__dirname, '..', '..', '..', 'assets', 'cross-spawn.cjs');
+const dest = path.join(__dirname, '..', '..', '..', 'assets');
+
+const BUILDS = [
+  { in: 'cross-spawn-6.0.5', out: 'cross-spawn.cjs', pre: path.join(__dirname, '..', 'assets', 'pre.js'), post: path.join(__dirname, '..', 'assets', 'post.js'), replacements: [{ from: 'Object.assign', to: '_object_spread' }] }
+];
 
 import fs from 'fs';
-const REPLACEMENTS = [
-  { from: 'Object.assign', to: '_object_spread' }
-]
-function patch(callback) {
+function patch(build, callback) {
   try {
-    const shims = fs.readFileSync(path.join(__dirname, '..', 'assets', 'shims.js'), 'utf8');
-    let content = fs.readFileSync(dest, 'utf8');
-    content = REPLACEMENTS.reduce((m, r) => m.replace(new RegExp(r.from, 'g'), r.to), content)
-    fs.writeFileSync(dest, shims + content, 'utf8');
+    const outPath = path.join(dest, build.out);
+    const pre = build.pre ? fs.readFileSync(build.pre, 'utf8') : '';
+    const post = build.post ? fs.readFileSync(build.post, 'utf8') : '';
+    let content = fs.readFileSync(outPath, 'utf8');
+    if (build.replacements) content = build.replacements.reduce((m, r) => m.replace(new RegExp(r.from, 'g'), r.to), content)
+    fs.writeFileSync(outPath, pre + content + post, 'utf8');
     callback();
   } catch (err) {
     callback(err);
@@ -27,8 +31,12 @@ function patch(callback) {
 
 function build(callback) {
   const config = path.join(__dirname, 'rollup.config.mjs');
-  const args = ['rollup', '--config', config, '--input', src, '--file', dest];
-  spawn(args[0], args.slice(1), { cwd: cwd, stdio: 'inherit' }, (err) => err ? callback(err) : patch(callback));
+  const queue = new Queue();
+  BUILDS.forEach(build => {
+    const args = ['--config', config, '--input', resolve.sync(build.in), '--file', path.join(dest, build.out)];
+    queue.defer((cb) => spawn('rollup', args, { cwd: cwd, stdio: 'inherit' }, (err) => err ? cb(err) : patch(build, cb)))
+  })
+  queue.await(callback)
 }
 
 build((err) => {
