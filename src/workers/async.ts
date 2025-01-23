4b818789169cb98977b97b679b6457c7626a9e10
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-22 21:53:34 -0800

    Replace call-once-fn with on-one in async worker

--- src/workers/async.ts
+++ src/workers/async.ts
@@ -1,7 +1,11 @@
-import once from 'call-once-fn';
+import oo from 'on-one';
 import { spawnKeys } from '../constants';
 
 import type { ChildProcess, SpawnCallback, SpawnError, SpawnOptions, SpawnResult } from '../types';
+
+function isError(obj) {
+  return Object.prototype.toString.call(obj) === '[object Error]';
+}
 
 export default function worker(cp: ChildProcess, options?: SpawnOptions | SpawnCallback, callback?: SpawnCallback) {
   if (typeof options === 'function') {
@@ -9,7 +13,6 @@ export default function worker(cp: ChildProcess, options?: SpawnOptions | SpawnC
     options = {};
   }
   options = options || {};
-  callback = once(callback); // some versions of node emit both an error and close
 
   // collect output
   const res = { pid: 0, output: [], stdout: null, stderr: null, status: null, signal: null } as SpawnResult;
@@ -18,34 +21,38 @@ export default function worker(cp: ChildProcess, options?: SpawnOptions | SpawnC
   if (options.encoding && cp.stdout) cp.stdout.on('data', stdout.push.bind(stdout));
   if (options.encoding && cp.stderr) cp.stderr.on('data', stderr.push.bind(stderr));
 
-  cp.on('error', (err: SpawnError) => err.code === 'OK' || callback(err));
-  cp.on('close', (status, signal) => {
-    setTimeout(function close() {
-      // prepare result
-      res.pid = cp.pid;
-      res.status = status;
-      res.signal = signal;
-      if (options.encoding && cp.stdout) {
-        res.stdout = Buffer.concat(stdout);
-        if (options.encoding !== 'binary') res.stdout = res.stdout.toString(options.encoding);
-      }
-      if (options.encoding && cp.stderr) {
-        res.stderr = Buffer.concat(stderr);
-        if (options.encoding !== 'binary') res.stderr = res.stderr.toString(options.encoding);
-      }
-      res.output = [null, res.stdout, res.stderr];
-      if (res.status === null) res.status = 0; // patch: early node on windows could return null
+  // some versions of node emit both an error and close
+  oo(cp, ['error', 'close'], (status, signal) => {
+    if (isError(status)) {
+      const err = status as SpawnError;
+      if (err.code === 'OK') return; // ignore
+      return callback(err);
+    }
 
-      // process errors
-      const err = res.status !== 0 ? new Error(`Non-zero exit code: ${res.status}`) : null;
-      if (err) {
-        for (const key in res) {
-          if (spawnKeys.indexOf(key) < 0) continue;
-          err[key] = Buffer.isBuffer(res[key]) ? res[key].toString(options.encoding || 'utf8') : res[key];
-        }
+    // prepare result
+    res.pid = cp.pid;
+    res.status = status;
+    res.signal = signal;
+    if (options.encoding && cp.stdout) {
+      res.stdout = Buffer.concat(stdout);
+      if (options.encoding !== 'binary') res.stdout = res.stdout.toString(options.encoding);
+    }
+    if (options.encoding && cp.stderr) {
+      res.stderr = Buffer.concat(stderr);
+      if (options.encoding !== 'binary') res.stderr = res.stderr.toString(options.encoding);
+    }
+    res.output = [null, res.stdout, res.stderr];
+    if (res.status === null) res.status = 0; // patch: early node on windows could return null
+
+    // process errors
+    const err = res.status !== 0 ? new Error(`Non-zero exit code: ${res.status}`) : null;
+    if (err) {
+      for (const key in res) {
+        if (spawnKeys.indexOf(key) < 0) continue;
+        err[key] = Buffer.isBuffer(res[key]) ? res[key].toString(options.encoding || 'utf8') : res[key];
       }
-      err ? callback(err) : callback(null, res);
-    }, 0);
+    }
+    err ? callback(err) : callback(null, res);
   });
 
   // pipe input
