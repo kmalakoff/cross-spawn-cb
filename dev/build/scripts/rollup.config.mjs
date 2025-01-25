474fff809f8dc6b99d78284b599cefafa0e63992
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-24 17:25:32 -0800

    Migrate asset replacement logic to Rollup plugin

--- dev/build/scripts/rollup.config.mjs
+++ dev/build/scripts/rollup.config.mjs
@@ -3,7 +3,19 @@ import resolve from '@rollup/plugin-node-resolve';
 import externals from 'rollup-plugin-node-externals';
 import swc from 'ts-swc-rollup-plugin';
 
+const replacements = {}
+replacements['Object.assign'] = 'objectAssign';
+replacements['Object.keys(env).find('] = 'findKey(env,';
+replacements['path.delimiter'] = 'pathDelimiter';
+replacements['cp.spawnSync'] = 'cpSpawnSync';
+
+const escape = (string) => string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+const replace = (replacements) => ({
+  name: 'replace',
+  transform: (code) => Object.keys(replacements).reduce((m, r) => m.replace(new RegExp(escape(r), 'g'), replacements[r]), code)
+})
+
 export default {
-  output: { format: 'cjs' },
-  plugins: [resolve(), commonjs(), externals({ deps: false, devDeps: false, builtinsPrefix: 'strip' }), swc()],
+  output: { format: 'cjs', strict: false },
+  plugins: [replace(replacements), resolve(), commonjs(), externals({ deps: false, devDeps: false, builtinsPrefix: 'strip' }), swc()],
 };
