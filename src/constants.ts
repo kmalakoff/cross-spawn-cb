727d3225dd94b2df0e52d6f48aabc8a0cceacf6f
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-16 00:14:17 -0800

    Improve command resolution and Windows detection

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
 export const spawnKeys = ['pid', 'status', 'signal', 'stdout', 'stderr', 'output'];
+
+export const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
