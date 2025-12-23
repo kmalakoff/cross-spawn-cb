ec82b2d696281f3a2ecd90c0b7c972fe7c04aa2d
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-23 09:29:13 -0800

    Upgrade modules

--- src/shim/spawnSync.ts
+++ src/shim/spawnSync.ts
@@ -10,7 +10,10 @@ const workerPath = path.join(__dirname, '..', '..', 'cjs', 'spawn.js');
 function run(cmd: string, args: string[], options?: object): SpawnResult {
   return cp.spawnSync(cmd, args, options) as SpawnResult;
 }
-const worker = typeof cp.spawnSync === 'function' ? run : bindSync(process.version, workerPath, { callbacks: true });
+
+type spawnSyncFunction = (cmd: string, args: string[], options?: object) => SpawnResult;
+
+const worker = (typeof cp.spawnSync === 'function' ? run : bindSync(process.version, workerPath, { callbacks: true })) as spawnSyncFunction;
 
 export default function spawnSync(cmd: string, args: string[], options?: SpawnOptions): SpawnResult {
   return worker(cmd, args, options || {}) as SpawnResult;
