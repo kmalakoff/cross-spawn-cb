2f514a9c24938ec83af3903629a1f2bb02c0d486
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-12 07:16:55 -0700

    Add string option to stdout and rename exitCode to code

--- test/spec/cli.test.js
+++ test/spec/cli.test.js
@@ -3,10 +3,20 @@ var assert = require('assert');
 var spawn = require('../..');
 
 describe('cli', function () {
-  it('basic command', function (done) {
+  it('stdio inherit', function (done) {
     spawn('node', ['--version'], { stdio: 'inherit' }, function (err, res) {
       assert.ok(!err);
-      assert.equal(res.exitCode, 0);
+      assert.equal(res.code, 0);
+      assert.equal(typeof res.stdout, 'undefined');
+      done();
+    });
+  });
+
+  it('stdout string', function (done) {
+    spawn('node', ['--version'], { stdout: 'string' }, function (err, res) {
+      assert.ok(!err);
+      assert.equal(res.code, 0);
+      assert.equal(typeof res.stdout, 'string');
       done();
     });
   });
