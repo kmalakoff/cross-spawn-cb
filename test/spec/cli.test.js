c732b49e9a25bcce3dd8ac887c596489894bce02
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-12 07:19:50 -0700

    Use end-of-stream and update dependencies

--- test/spec/cli.test.js
+++ test/spec/cli.test.js
@@ -3,21 +3,38 @@ var assert = require('assert');
 var spawn = require('../..');
 
 describe('cli', function () {
-  it('stdio inherit', function (done) {
-    spawn('node', ['--version'], { stdio: 'inherit' }, function (err, res) {
-      assert.ok(!err);
-      assert.equal(res.code, 0);
-      assert.equal(typeof res.stdout, 'undefined');
-      done();
+  describe('happy path', function () {
+    it('stdio inherit', function (done) {
+      spawn('node', ['--version'], { stdio: 'inherit' }, function (err, res) {
+        assert.ok(!err);
+        assert.equal(typeof res.stdout, 'undefined');
+        done();
+      });
+    });
+
+    it('stdout string', function (done) {
+      spawn('node', ['--version'], { stdout: 'string' }, function (err, res) {
+        assert.ok(!err);
+        assert.equal(typeof res.stdout, 'string');
+        done();
+      });
     });
   });
 
-  it('stdout string', function (done) {
-    spawn('node', ['--version'], { stdout: 'string' }, function (err, res) {
-      assert.ok(!err);
-      assert.equal(res.code, 0);
-      assert.equal(typeof res.stdout, 'string');
-      done();
+  describe('unhappy path', function () {
+    it('stdio inherit', function (done) {
+      spawn('node', ['--versio'], { stdio: 'inherit' }, function (err) {
+        assert.ok(err);
+        done();
+      });
+    });
+
+    it('stdout string', function (done) {
+      spawn('node', ['--versio'], { stderr: 'string' }, function (err) {
+        assert.ok(err);
+        assert.equal(typeof err.stderr, 'string');
+        done();
+      });
     });
   });
 });
